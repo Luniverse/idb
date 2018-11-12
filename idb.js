c2a765dba1b266a779feec39973ede3399367c3a
Lukas Jans <lukas@luniverse.de>
2018-11-12 18:18:20 +0100

    Classify library

--- idb.js
+++ idb.js
@@ -1,55 +1,73 @@
 /*!
- * idb.js IndexedDB wrapper v1.0
+ * idb.js IndexedDB wrapper v2.0
  * Licensed under the MIT license
  * Copyright (c) 2018 Lukas Jans
- * https://luniverse.de/
+ * https://github.com/Luniversity/idb
  */
-function IDB(setup) {
-	
-	// Connect to DB
-	let connection = new Promise((resolve, reject) => {
-		var request = indexedDB.open(setup.name, setup.version || 1);
+class IDB {
+	static connect(setup) {
 		
-		// Reject or resolve connection request
-		request.onerror = () => reject(request.error);
-		request.onsuccess = () => resolve(request.result);
+		// Connect to DB
+		this.connection = new Promise((resolve, reject) => {
+			const request = indexedDB.open(setup.name, setup.version || 1);
+			
+			// Reject or resolve connection request
+			request.onerror = () => reject(request.error);
+			request.onsuccess = () => resolve(request.result);
 
-		// Perform upgrade
-		request.onupgradeneeded = () => {
-			var db = request.result;
-			
-			// Clear old tables
-			for(var name of db.objectStoreNames) db.deleteObjectStore(name);
-			
-			// Create new tables
-			for(var table of setup.tables) db.createObjectStore(table.name, table.options);
-		}
-	});
-	
-	// Table controller
-	let Table = function(name) {
-		
-		// Start transaction
-		let transaction = () => new Promise((resolve, reject) => {
-			connection.then(db => {
-				resolve(db.transaction(name, 'readwrite').objectStore(name))
-			})
+			// Perform upgrade
+			request.onupgradeneeded = () => {
+				const db = request.result;
+				
+				// Clear old tables
+				for(const name of db.objectStoreNames) db.deleteObjectStore(name);
+				
+				// Create new tables
+				for(const table of setup.tables) db.createObjectStore(table.name, table.options);
+			}
 		});
 		
-		// Default promise from request
-		let promise = request => new Promise((resolve, reject) => {
+		// Bind table controllers
+		for(const table of setup.tables) this[table.name] = new IDB.Table(table.name);
+	}
+}
+
+// Table controller
+IDB.Table = class {
+	constructor(name) {
+		this.name = name;
+	}
+	
+	// Start new transaction
+	transaction() {
+		return new Promise((resolve, reject) => {
+			IDB.connection.then(db => {
+				resolve(db.transaction([this.name], 'readwrite').objectStore(this.name));
+			});
+		});
+	}
+	
+	// Wrap request in promise
+	promise(request) {
+		return new Promise((resolve, reject) => {
 			request.onerror = () => reject(request.error);
 			request.onsuccess = () => resolve(request.result);
 		});
-		
-		// Insert data
-		this.set = (index, value) => transaction().then(table => promise(table.put(value, index)));
-		
-		// Select data by index
-		this.get = index => transaction().then(table => promise(table.get(index)));
-		
-		// Select all data
-		this.all = () => transaction().then(table => new Promise((resolve, reject) => {
+	}
+	
+	// Insert data
+	set(index, value) {
+		return this.transaction().then(table => this.promise(table.put(value, index)));
+	}
+	
+	// Select data by index
+	get(index) {
+		return this.transaction().then(table => this.promise(table.get(index)));
+	}
+	
+	// Select all data
+	all() {
+		return this.transaction().then(table => new Promise((resolve, reject) => {
 			var rows = {};
 			var request = table.openCursor();
 			request.onerror = () => reject(request.error);
@@ -63,26 +81,31 @@ function IDB(setup) {
 				} else resolve(rows);
 			}
 		}));
-		
-		// Select data by condition
-		this.where = condition => this.all().then(rows => {		
+	}
+	
+	// Select data by condition
+	where(condition) {
+		return this.all().then(rows => {	
 			var filtered = {};
 			for(var index in rows) {
 				if(condition(rows[index])) filtered[index] = rows[index];
 			}
 			return filtered;
 		});
-		
-		// Clear data
-		this.clear = () => transaction().then(table => promise(table.clear()));
-		
-		// Delete entry by index
-		this.delete = index => transaction().then(table => promise(table.delete(index)));
-		
-		// Add new entry
-		this.add = value => transaction().then(table => promise(table.put(value)));
 	}
 	
-	// Create table controllers
-	for(var table of setup.tables) this[table.name] = new Table(table.name);
+	// Clear data
+	clear() {
+		return this.transaction().then(table => this.promise(table.clear()));
+	}
+	
+	// Delete entry by index
+	delete(index) {
+		return this.transaction().then(table => this.promise(table.delete(index)));
+	}
+	
+	// Add new entry
+	add(value) {
+		return this.transaction().then(table => this.promise(table.put(value)));
+	}
 }
